0d76f669db81f88842cda70b21f6c2668f4f8c74
Matthieu Cesar Bonnardot <matthieubonnardot88@gmail.com>
2021-01-21 11:21:18 +0100

    Added Default values to the set methods to avoid breaks

--- metronome.js
+++ metronome.js
@@ -3,7 +3,7 @@ const DEFAULT_SCHEDULE_S = 0.1;
 const DEFAULT_BPM = 90;
 const DEFAULT_TIME_SIGNITURE = 16;
 const DEFAULT_NOTE_LENGTH = 0.05;
-const DEFAULT_OSCILLATOR_TYPE = 'sine';
+const DEFAULT_OSCILLATOR_TYPE = "sine";
 const DEFAULT_FREQUENCY = 220;
 const MIN_FREQUENCY = 40;
 const MINUTE = 15;
@@ -13,12 +13,12 @@ const AudioContext = window.AudioContext || window.webkitAudioContext; //maybe c
 const audioCtx = new AudioContext();
 
 class Metronome {
-
   constructor(
     BPM = DEFAULT_BPM,
     timeSigniture = DEFAULT_TIME_SIGNITURE,
-    gain = DEFAULT_GAIN) {
-      //User mutable
+    gain = DEFAULT_GAIN
+  ) {
+    //User mutable
     this._BPM = BPM;
     this._timeSigniture = timeSigniture;
     this._gain = gain;
@@ -28,12 +28,12 @@ class Metronome {
     this._oscillatorType = DEFAULT_OSCILLATOR_TYPE;
     this._frequency = DEFAULT_FREQUENCY;
 
-      //Functionally assigned
+    //Functionally assigned
     this._samplesArray = [];
     this._samplesLoaded = false;
     this._accentChecked = false;
 
-      //Internal values
+    //Internal values
     this._notesInQueue = [];
     this._playing = false;
     this._timerID;
@@ -41,15 +41,14 @@ class Metronome {
     this._currentNote = 0;
     this._lookahead = DEFAULT_LOOKAHEAD_MS;
     this._scheduleAheadTime = DEFAULT_SCHEDULE_S;
-
-  };
+  }
 
   //Client settings ----------------------------------------
-  set BPM(newBPM) {
+  set BPM(newBPM = DEFAULT_BPM) {
     this._BPM = Number(newBPM);
   }
 
-  set timeSigniture(newTimeSigniture) {
+  set timeSigniture(newTimeSigniture = DEFAULT_TIME_SIGNATURE) {
     this._timeSigniture = Number(newTimeSigniture);
   }
 
@@ -58,44 +57,51 @@ class Metronome {
   }
 
   set noteVolumes(volumesArray) {
-    this._noteVolumes = volumesArray;
+    if (volumesArray) this._noteVolumes = volumesArray;
+    else console.error("No array of volumes has been passed.");
   }
 
   set budge(time) {
-    this._pushNote = time;
+    if (time) this._pushNote = time;
+    else console.error("No time has been passed.");
   }
 
   set noteLength(time) {
-    this._noteLength =  Number(time);
+    if (time) this._noteLength = Number(time);
+    else console.error("No time has been passed.");
   }
 
   set oscillatorType(wave) {
-    let newWaveType;
-    switch (wave) {
-      case 'sine':
-      case 'square':
-      case 'sawtooth':
-      case 'triangle':
-        newWaveType = wave;
-        break;
-      default:
-        newWaveType = DEFAULT_OSCILLATOR_TYPE;
+    if (wave) {
+      let newWaveType;
+      switch (wave) {
+        case "sine":
+        case "square":
+        case "sawtooth":
+        case "triangle":
+          newWaveType = wave;
+          break;
+        default:
+          newWaveType = DEFAULT_OSCILLATOR_TYPE;
+      }
+      this._oscillatorType = newWaveType;
+    } else {
+      console.error("No wave value has been passed.");
     }
-    this._oscillatorType = newWaveType;
   }
 
-  set frequency(freq) {
+  set frequency(freq = DEFAULT_FREQUENCY) {
     if (freq <= MIN_FREQUENCY) freq = MIN_FREQUENCY;
     this._frequency = freq;
   }
 
-  set gain(gain) {
+  set gain(gain = DEFAULT_GAIN) {
     this._noteVolumes = new Array(this._timeSigniture).fill(gain);
   }
 
   _valueChecks() {
     while (this._noteVolumes.length < this._timeSigniture) {
-      this._noteVolumes = [...this._noteVolumes, ...this._noteVolumes]
+      this._noteVolumes = [...this._noteVolumes, ...this._noteVolumes];
     }
   }
 
@@ -124,27 +130,43 @@ class Metronome {
   }
 
   // This allows output of the notesInQueue array to sync with graphics
-  aListener(val) {};
+  aListener(val) {}
   registerListener(listener) {
     this.aListener = listener;
   }
 
   _scheduleSamples(beatNumber, time) {
-
     this._notesInQueue.push({ note: beatNumber, time: time });
-    this.aListener(this._notesInQueue, audioCtx.currentTime, this._samplesArray[0].name)
-    if (this._notesInQueue.length >= this._timeSigniture) this._notesInQueue.splice(0,1);
+    this.aListener(
+      this._notesInQueue,
+      audioCtx.currentTime,
+      this._samplesArray[0].name
+    );
+    if (this._notesInQueue.length >= this._timeSigniture)
+      this._notesInQueue.splice(0, 1);
     if (this._accentChecked && this._samplesArray.length >= 2) {
-      if (beatNumber === this._timeSigniture - 1) this._playSample(audioCtx, this._samplesArray[1].audioBuffer, this._noteVolumes[beatNumber]);
-      else this._playSample(audioCtx, this._samplesArray[0].audioBuffer, this._noteVolumes[beatNumber]);
+      if (beatNumber === this._timeSigniture - 1)
+        this._playSample(
+          audioCtx,
+          this._samplesArray[1].audioBuffer,
+          this._noteVolumes[beatNumber]
+        );
+      else
+        this._playSample(
+          audioCtx,
+          this._samplesArray[0].audioBuffer,
+          this._noteVolumes[beatNumber]
+        );
     } else {
-      this._playSample(audioCtx, this._samplesArray[0].audioBuffer, this._noteVolumes[beatNumber]);
+      this._playSample(
+        audioCtx,
+        this._samplesArray[0].audioBuffer,
+        this._noteVolumes[beatNumber]
+      );
     }
-
   }
 
   _scheduleOscillator(beatNumber, time) {
-
     this._notesInQueue.push({ note: beatNumber, time: time });
 
     const oscillator = audioCtx.createOscillator();
@@ -155,69 +177,81 @@ class Metronome {
     oscillator.type = this._oscillatorType;
 
     if (this._noteVolumes[beatNumber] === 0) {
-      gainNode.gain.setValueAtTime(0, audioCtx.currentTime)
+      gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
     }
     if (this._accentChecked) {
-      if (beatNumber === this._timeSigniture - 1) oscillator.frequency.value = this._frequency * 2;
+      if (beatNumber === this._timeSigniture - 1)
+        oscillator.frequency.value = this._frequency * 2;
       else oscillator.frequency.value = this._frequency;
     } else if (!this._accentChecked) {
       oscillator.frequency.value = this._frequency;
     }
 
     // if (this._noteVolumes[beatNumber] != 0) {
-      oscillator.start(time + this._pushNote);
-      oscillator.stop(time + this._noteLength + this._pushNote);
+    oscillator.start(time + this._pushNote);
+    oscillator.stop(time + this._noteLength + this._pushNote);
     // }
   }
 
   _scheduler() {
-
     let context;
     if (!context) context = this;
 
     function contextScheduler() {
-      while (context._nextNoteTime < audioCtx.currentTime + context._scheduleAheadTime) {
-        if (context._samplesLoaded) context._scheduleSamples(context._currentNote, context._nextNoteTime);
-        else context._scheduleOscillator(context._currentNote, context._nextNoteTime);
+      while (
+        context._nextNoteTime <
+        audioCtx.currentTime + context._scheduleAheadTime
+      ) {
+        if (context._samplesLoaded)
+          context._scheduleSamples(context._currentNote, context._nextNoteTime);
+        else
+          context._scheduleOscillator(
+            context._currentNote,
+            context._nextNoteTime
+          );
         context._nextNote();
       }
-      context._timerID = window.setTimeout(contextScheduler, context._lookahead);
+      context._timerID = window.setTimeout(
+        contextScheduler,
+        context._lookahead
+      );
     }
 
-    contextScheduler()
-
+    contextScheduler();
   }
 
   //Deal with custom samples
 
   loadSamples(urlArray) {
-    this._setUpSample(urlArray)
-      .then(samples => {
-        this._samplesArray = [...samples];
-        this._samplesLoaded = true;
-      })
+    this._setUpSample(urlArray).then((samples) => {
+      this._samplesArray = [...samples];
+      this._samplesLoaded = true;
+    });
   }
 
   async _loadSound(audioCtxParam, filePath) {
     try {
-    const response = await fetch(filePath);
-    const arrayBuffer = await response.arrayBuffer()
-    const audioBuffer = await audioCtxParam.decodeAudioData(arrayBuffer);
-    return audioBuffer;
+      const response = await fetch(filePath);
+      const arrayBuffer = await response.arrayBuffer();
+      const audioBuffer = await audioCtxParam.decodeAudioData(arrayBuffer);
+      return audioBuffer;
     } catch (e) {
       console.log(e);
     }
-  };
+  }
 
   async _setUpSample(urlArray) {
-    return Promise.all(urlArray.map(async path => {
-      let sampleHolder = {};
-      sampleHolder.audioBuffer = await this._loadSound(audioCtx, path);
-      sampleHolder.name = path.match(/([^\/]+)(?=\.\w+$)/)[0].replace(/-/, '_');
-      return sampleHolder;
-    }))
-    .then(data => data);
-  };
+    return Promise.all(
+      urlArray.map(async (path) => {
+        let sampleHolder = {};
+        sampleHolder.audioBuffer = await this._loadSound(audioCtx, path);
+        sampleHolder.name = path
+          .match(/([^\/]+)(?=\.\w+$)/)[0]
+          .replace(/-/, "_");
+        return sampleHolder;
+      })
+    ).then((data) => data);
+  }
 
   _playSample(audioCtxParam, audioBuffer, noteVolume = 1) {
     const sampleSource = audioCtxParam.createBufferSource();
@@ -229,7 +263,6 @@ class Metronome {
     sampleSource.start();
     return sampleSource;
   }
-
 }
 
 export default Metronome;
