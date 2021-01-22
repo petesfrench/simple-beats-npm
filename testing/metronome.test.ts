bd8aa02d593cc3251c1cfd29e6e95c6ef2eb1aaa
Alexandra <donchenkoalexandra@gmail.com>
2021-01-22 15:44:20 +0100

    jsdom required and setted up

--- testing/metronome.test.ts
+++ testing/metronome.test.ts
@@ -1,5 +1,15 @@
 import Metronome from "../metronome";
 
+require('jsdom-global')();
+const fs = require('fs');
+const options: object = {
+    resources: 'useble', 
+    runScripts: 'dangerously',
+    url:'file://'+ __dirname
+}
+const html = fs.readFileSync(__dirname + '/../index.html', 'utf8');
+require('jsdom-global')(html, options);
+
 const metronome = require('../metronome');
 
 test('create instance of a metronome', () => {
